878c57c4d0739dfeb3f0f8fa49b9522ac54c8a95
techmahedy <mahedy150101@gmail.com>
2021-03-28 14:43:13 +0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+     <Nav/>
+      <div>
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay 
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      /> 
+    </div>
+   </div>
+</template>
+
+<script>
+import Hero from '../components/Hero.vue'
+import LargeCardDisplay from '../components/LargeCardDisplay.vue'
+import { largeCardSections , smallCardSections } from '@/assets/data.js'
+import Nav from '../components/Nav.vue'
+
+export default {
+  components: { LargeCardDisplay, Hero, Nav },
+  data(){
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <Nav/>
+        <LargeCardDisplay
+            v-for="cardInfo in largeCardInfo.slice(0,1)"
+            :key="cardInfo.id"
+            :cardsSection="cardInfo"
+        />
+        <SmallCardDisplay 
+            v-for="cardInfo in smallCardSections"
+            :key="cardInfo.id"
+            :cardsSection="cardInfo"
+        /> 
+    </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js"
+import Nav from '../../components/Nav.vue'
+    export default {
+    components: { Nav },
+        data(){
+            return {
+                largeCardInfo: largeCardSections,
+                smallCardSections: smallCardSections
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
